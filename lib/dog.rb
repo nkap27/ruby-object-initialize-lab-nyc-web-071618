2f639018f0a1079afa35062b4fa5fd989e8dc54e
Natasha K <kapr.natasha@gmail.com>
2018-07-18 14:56:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,3 +5,8 @@ class Dog
   end
 
 end
+
+#initialize
+    # sets the name of the dog in an instance variable @name
+    # sets the breed of the dog in an instance variable @breed
+    # defaults the breed argument to "Mutt" in an instance variable @breed
